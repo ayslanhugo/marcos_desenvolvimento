dff32a3065de3b5e4a8b6f117868833294c10e90
Ayslan Hugo <ayslanhugo1@gmail.com>
2026-03-17 21:41:45 -0300

    feat: implementa cards de progresso e modal de avaliação, substitui lógica de exibição e adiciona feedback visual ao salvar progresso

--- app/controllers/progresses_controller.rb
+++ app/controllers/progresses_controller.rb
@@ -5,19 +5,14 @@ class ProgressesController < ApplicationController
   def create
     @progress = @child.progresses.find_or_initialize_by(milestone: @milestone)
 
-    if @progress.update(progress_params)
-      # Mantemos a inteligência clínica que você já tinha construído!
-      if @progress.red_flag?
-        flash[:alert] = "Atenção: Este marco sugere uma avaliação com o pediatra."
+    respond_to do |format|
+      if @progress.update(progress_params)
+        format.turbo_stream
+        format.html { redirect_to child_path(@child), notice: "Progresso salvo!" }
       else
-        flash[:notice] = "Progresso salvo com sucesso!"
+        format.html { redirect_to child_path(@child), alert: "Erro ao salvar." }
       end
-    else
-      # Se der erro, agora o Rails vai nos contar exatamente o porquê:
-      flash[:alert] = "Não foi possível salvar o progresso: #{@progress.errors.full_messages.to_sentence}"
     end
-
-    redirect_to child_path(@child)
   end
 
   private
@@ -31,7 +26,6 @@ class ProgressesController < ApplicationController
   end
 
   def progress_params
-    # Adicionamos o :milestone_id aqui para garantir que o Rails não bloqueie o ID que vem do Modal
     params.require(:progress).permit(:status, :notes, :milestone_id)
   end
 end
