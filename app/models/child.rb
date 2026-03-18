f431d63cc1846fb4cd8c8da853a6cf4a26ccc9a4
Ayslan Hugo <ayslanhugo1@gmail.com>
2026-03-17 22:08:47 -0300

    feat: adiciona métodos para contagem de marcos e atualiza a exibição de progresso geral com feedback visual

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,19 +1,23 @@
 class Child < ApplicationRecord
-  # Relacionamentos
   belongs_to :user
-  # Uma criança tem muitos registros de progresso. Se a criança for excluída, os progressos também serão.
   has_many :progresses, dependent: :destroy
 
-  # Definindo os tipos de desenvolvimento (Enum)
   enum :tipo_desenvolvimento, { tipico: 0, sindrome_down: 1 }
 
-  # Validações básicas
   validates :nome, :data_nascimento, :tipo_desenvolvimento, presence: true
 
-  # Método para calcular a idade em meses
-  # Ele precisa estar aqui dentro para que possamos fazer @child.idade_em_meses
   def idade_em_meses
     hoje = Date.today
     (hoje.year * 12 + hoje.month) - (data_nascimento.year * 12 + data_nascimento.month)
+  end
+  def milestones_count_for_type
+    Milestone.where(tipo_desenvolvimento: self.tipo_desenvolvimento).count
+  end
+
+  def achieved_milestones_count
+    progresses.joins(:milestone)
+              .where(milestones: { tipo_desenvolvimento: self.tipo_desenvolvimento })
+              .where(status: "adquirido")
+              .count
   end
 end
